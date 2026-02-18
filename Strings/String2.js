c4c7b9c71210ef5a6c8c3bbbd9feaac4c9d137c6
vijaymk1f3-del <vijaymk1f3@gmail.com>
2026-02-18 21:37:57 +0530

    feat:Created String2.js in which added functions related to Strings

---
+++ Strings/String2.js
@@ -0,0 +1,15 @@
+let b="Shivam121"
+
+console.log("Strings are Unchangable")
+
+console.log("Shivam121 in Upper case "+b.toUpperCase())
+console.log("Shivam121 in Lower Case "+b.toLowerCase())
+console.log("Shivam121's length "+ b.length)
+console.log("Shivam121's name from index 1 to 4 "+b.slice(1,5))
+console.log("Shivam121's name from index 1 to last "+b.slice(1))
+console.log("In Shivam121 replace 121 by 123 "+b.replace("121","123"))
+console.log("Shivam121 concat itself "+b.concat(b))
+console.log("Shivam121 concat itself with space "+b.concat(" ",b))
+console.log("Shivam121 remove spaces for start and end "+b.concat("  1  ".trim()))
+
+
