7eb3c9161e5739a5b2143c51b8e116ca97931a45
vijaymk1f3-del <vijaymk1f3@gmail.com>
2026-02-18 20:58:54 +0530

    I created Strings forlder and a String1.js file

---
+++ Strings/String1.js
@@ -0,0 +1,12 @@
+console.log("This is about strings");
+let a="Sanga121"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+console.log(a[8])
+console.log(a[9])
